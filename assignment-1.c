cd40c93f2819ae8cb180439b20afd5515afec73f
Md Zeeshan <imerzeeshan@gmail.com>
2024-09-18 22:53:29 +0530

    assignment 1

---
+++ assignment-1.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+int main(){
+  /*  //1
+    printf("\"MySirG\"\n");
+
+    //2
+    int radius;
+    float pi=3.14,area;
+    printf("Enter the value of Radius\n");
+    scanf("%d",&radius);
+    printf("\"Area of Circle is %f having the radius %d\". \n",area=(pi*radius*radius),radius);
+  
+
+    //3
+    char c[30];
+    int x;
+
+    printf("Enter the string to calculate length\n");
+     scanf("%s",c);
+    //x=strlen(c);
+    printf("length of the string = %d \n",strlen(c));
+
+    
+
+    printf("Enter the string to calculate length\n");
+     scanf("%s",c);
+    for (int i = 0; c[i] !='\0'; ++i)
+    {
+    printf("length of the string = %d \n",i);
+    }
+     
+     //6
+      char str[30];
+
+    printf("Enter Your name\n");
+     scanf("%s",str);
+    
+    printf("Your Name in Double Quotes \" %s \" \n",str);
+    
+
+    //7
+    char y='%';
+    char x='d';
+    printf("\"%c%c\"",y,x);
+     
+
+    //8
+    char ch='n';
+    printf("\"\\%c\"",ch);
+   
+   //9
+   printf("\"\\\\\"");
+
+    */
+
+
+   //10
+   int d,m,y;
+   char ch;
+   printf("enter the date as formate (DD/MM/YYYY)");
+   scanf("%d%s%d%s%d",&d,ch,&m,ch,&d);
+   printf("The Date Enter By User :%d%s%d%s%d ",d,ch,m,ch,y);
+}
