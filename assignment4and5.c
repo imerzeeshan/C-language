d4308cec6711ee36c4e2f3df22f05cc227e1186c
Md Zeeshan <imerzeeshan@gmail.com>
2024-09-20 19:08:32 +0530

    assignment 4 and 5

---
+++ assignment4and5.c
@@ -0,0 +1,101 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+
+    //1 
+    /*int num;
+    printf("enter a nunber how much line you want to print : ");
+    scanf("%d",&num);
+    for(int i=1; i<=num; i++)
+        printf("\n MySirG \n");*/
+
+
+    
+    //2
+    /*int num1;
+    printf("Enter a number as far as you want to print : ");
+    scanf("%d",&num1);
+
+    for(int i=1;i<=num1;i++)
+        printf("%d \t",i);*/
+
+
+
+    //3
+    /*int num2;
+    printf("Enter a number as far as you want to print : ");
+    scanf("%d",&num2);
+
+    for(int i=1;i<=num2;i++)
+        printf("%d \t",num2-i+1);*/
+
+
+    
+    //4
+    /*int num3;
+    printf("Enter a number as far as you want to print odd number : ");
+    scanf("%d",&num3);
+
+    for(int i=1;i<=num3;i+=2)
+        printf("%d \t",i);*/
+
+
+
+    //5
+    /*int num4;
+    printf("Enter a number as far as you want to print odd number : ");
+    scanf("%d",&num4);
+
+    for(int i=1;i<=num4;i+=2)
+        printf("%d \t",num4-i);*/
+
+
+    //6
+    /*int num5;
+    printf("Enter a number as far as you want to print even number : ");
+    scanf("%d",&num5);
+
+    for(int i=1;i<=num5;i++)
+        if(i%2==0)
+        printf("%d \t",i);*/
+
+    
+
+    //7
+    /*int num6;
+    printf("Enter a number as far as you want to print even number in reverse order: ");
+    scanf("%d",&num6);
+
+    for(int i=1;i<=num6;i++)
+       if(i%2==0)
+        printf("%d \t",num6-i+2);*/
+
+
+
+    //8
+    /*int num7;
+    printf("Enter a number as far as you want to print natural number in squares : ");
+    scanf("%d",&num7);
+
+    for(int i=1;i<=num7;i++)
+        printf("%d \t",i*i);*/
+
+
+    //9
+    /*int num8;
+    printf("Enter a number as far as you want to print natural number in cubes : ");
+    scanf("%d",&num8);
+
+    for(int i=1;i<=num8;i++)
+        printf("%d \t",i*i*i);*/
+
+
+    //10
+    int num9;
+    printf("Enter a number you want to print a table : ");
+    scanf("%d",&num9);
+
+    for(int i=1;i<=10;i++)
+        printf("%d * %d = %d \n",num9,i,i*num9);
+    
+}
